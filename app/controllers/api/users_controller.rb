5e857ecfb87844299a0c6aeedcae35cd8aed1be0
mattswedin <swedin.matthew@gmail.com>
2022-02-08 12:25:29 -0500

    sessions controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,18 +1,42 @@
 class UsersController < ApplicationController
+
+    before_action :require_logged_in, except [:create]
+
     def create
         @user = User.new(user_params)
         if @user.save
+           login!(@user)
            render :show 
+        else
+            render json: @user.errors.full_messages, status: 422
         end
     end
 
     def index
+        @users = User.all
+        render :index
     end
 
     def show
+        @user = User.find_by(id: params[:id])
+        render :show
+    end
+
+    def update
+        @user = User.find_by(id: params[:id])
+
+        if @user.update(user_update_params)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
     end
 
     private
+
+    def user_update_params
+        params.require(:user).permit(:level, :attack, :defense)
+    end
 
     def user_params
         params.require(:user).permit(:username, :password)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  namespace :api, defaults: {format: :json} do
+    resources :users, except: [:new]
+    resource :session, only: [:create, :destroy, :show]
+  end
+
   root 'static_pages#root' # (root = '/') static_pages controller #root 
   # when the url is only '/', we're gonna go to static_pages controller, hitting the root method
 
