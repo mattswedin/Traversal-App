bcd6d4110324b5d14f8f8562e54c67e6751671b2
mattswedin <swedin.matthew@gmail.com>
2022-02-08 20:18:04 -0500

    implement session util

---
+++ frontend/util/session_api_util.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+//session
+
+export const login = user => {
+    return axios.post('api/session', user)
+}
+
+export const logout = () => {
+    return axios.delete('api/session')
+}
+
+//user
+
+export const signup = user => {
+    return axios.post('api/users', user)
+}
+
+export const updateUser = user => {
+    return axios.patch(`api/users/${user.id}`, user)
+}
+
+export const showUser = user => {
+    return  axios.get(`api/users/${user.id}`)
+}
