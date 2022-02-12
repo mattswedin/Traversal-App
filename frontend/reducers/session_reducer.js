1e0ebbe973eb5f946ab682f8a745401b4569db10
mattswedin <swedin.matthew@gmail.com>
2022-02-12 15:21:08 -0500

    session reducer

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -3,20 +3,23 @@ import {
     LOGOUT_CURRENT_USER
 } from "../actions/session_actions.js"
 
-const sessionReducer = (oldState = {}, action) => {
+const initialState = {
+    isAuthenticated: false,
+    user: {}
+}
 
-    const initialState = {
-        isAuthenticated: false,
-        user: {}
-    }
-    
+const sessionReducer = (state = initialState, action) => {
+
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
             return { id: action.currentUser.id };
         case LOGOUT_CURRENT_USER:
-            return {}
+            return {
+                isAuthenticated: false,
+                user: undefined
+             };
         default:
-            return oldState;
+            return state;
     }
 }
 
