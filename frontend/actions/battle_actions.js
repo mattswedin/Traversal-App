c780b8acb10d039c4fad5e9c68e71888a70291f3
mattswedin <swedin.matthew@gmail.com>
2022-03-03 18:20:28 -0500

    implement Battle actions

---
+++ frontend/actions/battle_actions.js
@@ -0,0 +1,31 @@
+import * as BattleApiUtil from '../util/battle_api_util'
+
+export const RECEIVE_BATTLE = 'RECEIVE_BATTLE'
+export const RECEIVE_BATTLE_ERRORS = 'RECEIVE_BATTLE_ERRORS'
+
+export const receieveBattleErrors = error => ({
+    type: RECEIVE_BATTLE_ERRORS,
+    errors
+})
+
+export const receieveBattle = battle => ({
+    type: RECEIVE_BATTLE,
+    battle
+})
+
+export const updateBattle = battle => dispatch => (
+    BattleApiUtil.updateBattle(battle)
+    .then((battle) => {
+        dispatch(receieveBattle(battle))
+    })
+    .catch(err => {
+        dispatch(receieveBattleErrors(err.response.data))
+    })
+)
+
+export const showBattle = battleId => dispatch => (
+    BattleApiUtil.showBattle(battleId)
+    .then((battle) => {
+        dispatch(receieveBattle(battle))
+    })
+)
